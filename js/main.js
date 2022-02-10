5ccb2e5da1d1860975700817c1e6fab7ec39e4d0
Carl Jordan Duff <carljduff@gmail.com>
2022-02-10 00:43:26 +0000

    working clock

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ let seconds = today.getSeconds();
 let time = hour + ':' + minute + ":" + seconds; */
 // -----------------------------------------------------
 
+function updateTime() {
+    let today = new Date();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let seconds = today.getSeconds();
+    let time = hour + ':' + minute + ":" + seconds;
+
+    if (hour < 10 || minute < 10 || seconds < 10) {
+        document.getElementById('clock').innerHTML = "0" + hour + ":" + "0" + minute + ":" + "0" + seconds;
+    } else {
+        document.getElementById('clock').innerHTML = time;
+    }
+};
+setInterval(updateTime, 1000);
+
+document.getElementById('clock').onload(updateTime());
+
 // -------------------DATE-----------------------
 /*let today = new Date();
 let year = today.getFullYear();

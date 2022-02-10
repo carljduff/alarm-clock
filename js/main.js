078da54688dcadea36efd40b8137809f5a0e281e
Carl Jordan Duff <carljduff@gmail.com>
2022-02-10 00:04:27 +0000

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+var today = new Date();
+
+let hour = today.getHours();
+let minute = today.getMinutes();
+let seconds = today.getSeconds();
+
+
+let time = hour + ':' + minute + ":" + seconds;
+
+
+document.getElementById('clock').innerHTML = time;

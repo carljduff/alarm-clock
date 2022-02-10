e6a97e5aee1623ae9112bb5813772642d44314b9
Carl Jordan Duff <carljduff@gmail.com>
2022-02-10 00:34:02 +0000

    time and date

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-var today = new Date();
-
+// -------------------TIME-----------------------------
+/*let today = new Date();
 let hour = today.getHours();
 let minute = today.getMinutes();
 let seconds = today.getSeconds();
+let time = hour + ':' + minute + ":" + seconds; */
+// -----------------------------------------------------
+
+// -------------------DATE-----------------------
+/*let today = new Date();
+let year = today.getFullYear();
+let month = (today.getMonth() + 1); 
+let day = today.getDate();
+let date = month + "-" + day + "-" + year;*/
+//-------------------------------------------------------
 
 
-let time = hour + ':' + minute + ":" + seconds;
+// --------------No "0" before Month/Day----------------------- 
+/*if (month < 10 || day < 10) {
+    document.getElementById('date').innerHTML = "0" + month + "-" + "0" + day + "-" + year;
+} else {
+    document.getElementById('date').innerHTML = date;
+};*/
+// -----------------------------------------------------------------
 
 
-document.getElementById('clock').innerHTML = time;
+//------------------------- No "0" before time ------------------------
+/*if (hour < 10 || minute < 10 || seconds < 10) {
+    document.getElementById('clock').innerHTML = "0" + hour + ":" + "0" + minute + ":" + "0" + seconds;
+} else {
+    document.getElementById('clock').innerHTML = time;
+};*/
+// ------------------------------------------------------------------------
+
